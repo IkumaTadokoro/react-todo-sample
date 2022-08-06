507fe2d29759addfcd55fd509c4c5b488bec2763
ikuma-t <tadokorodev@gmail.com>
2022-08-06 12:05:13 +0900

    feat: eslintとprettierを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-const App = () => {
-  return (
-    <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <div className="flex flex-col items-start mb-6">
-          <label htmlFor="todo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Todo Name</label>
-          <input id="todo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" value="" onChange={(e) => e.preventDefault()}/>
-        </div>
-        <div className="flex justify-end mb-6">
-          <input className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" value="追加" onSubmit={(e) => e.preventDefault()}/>
-        </div>
-      </form>
-    </>
-  )
-}
+const App = () => (
+  <form onSubmit={(e) => e.preventDefault()}>
+    <div className="mb-6 flex flex-col items-start">
+      <label
+        htmlFor="todo"
+        className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+      >
+        Todo Name
+        <input
+          id="todo"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          type="text"
+          value=""
+          onChange={(e) => e.preventDefault()}
+        />
+      </label>
+    </div>
+    <div className="mb-6 flex justify-end">
+      <input
+        className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="submit"
+        value="追加"
+        onSubmit={(e) => e.preventDefault()}
+      />
+    </div>
+  </form>
+);
 
-export default App
+export default App;
